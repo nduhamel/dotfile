1b40080ed201a3b6c64baeb59091dbca66b66bdc
Nicolas Duhamel <nicolas@jombi.fr>
2018-04-23 00:29:11 +0200

    Use stow for installing

---
+++ README.md
@@ -0,0 +1,6 @@
+Install
+=======
+
+First you need to setup the base directory structure with `pre.sh`, after use stow for install user packages configurations:
+
+    stow -t ~/ package
---
+++ user/pre.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+###################################
+#
+# Create common directory structure
+#
+###################################
+
+mkdir $HOME/.config
+mkdir -p $HOME/.local/{bin,share}
