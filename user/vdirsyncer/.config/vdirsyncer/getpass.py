dc564c21ed905a3665b58e60e2eb94bf20e910f5
Nicolas Duhamel <nicolas@jombi.fr>
2018-04-29 13:39:18 +0200

    Add vdirsyncer

---
+++ user/vdirsyncer/.config/vdirsyncer/getpass.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python2
+import sys
+from subprocess import check_output
+
+
+def get_pass(account):
+    return check_output("pass nextcloud/" + account, shell=True).splitlines()[0]
+
+if __name__ == '__main__':
+    print get_pass(sys.argv[1])
