46d4295d1abc71beade33be3f0355d41a6d65cdd
Nicolas Duhamel <nicolas@jombi.fr>
2018-05-09 12:35:58 +0200

    Fix shell

--- user/environment/.local/bin/env-gen.sh
+++ user/environment/.local/bin/env-gen.sh
@@ -5,10 +5,8 @@
 # This script load them for you
 # source it from your shellrc
 
-if [ -d ~/.config/environment.d ]; then
-    set -a
-    for f in `ls -v ~/.config/environment.d`; do
-        . ~/.config/environment.d/$f
-    done
-fi
+[ -d ~/.config/environment.d ] || exit 0
 
+set -a
+for f in ~/.config/environment.d/*; do . $f; done
+set +a
